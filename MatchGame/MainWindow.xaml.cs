3b4372ef07f8442643cc9fa5356c879441ff2555
Jordan S <jordansantley@gmail.com>
2021-07-12 14:34:28 -0400

    new

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -70,7 +70,7 @@ namespace MatchGame
                 if (textBlock.Name != "timeTextBlock")
                 {
 
-
+                    textBlock.Visibility = Visibility.Visible;
                     int index = random.Next(animalEmogi.Count);
                     string nextEmogi = animalEmogi[index];
                     textBlock.Text = nextEmogi;
